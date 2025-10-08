ff9b5b149b25aed2c929a23c9d273ae8cd7683a7
Erica Nyman <ervin.erica29@gmail.com>
2025-10-07 22:21:23 -0400

    feat:App.jsx push

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-<<<<<<< Updated upstream
-import { Route } from "react-router";
-import { Routes } from "react-router";
-=======
 import { Routes, Route } from "react-router";
 // import BookDetail from "./reservations/BookDetail";
 import BookList from "./catalog/list";
 import Layout from "./catalog/Layout";
 import Register from "./auth/Register";
->>>>>>> Stashed changes
 
 export default function App() {
   return (
     <>
-<<<<<<< Updated upstream
-      <h1>App</h1>
-=======
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/register" element={<Register />} />
@@ -23,7 +15,6 @@ export default function App() {
           </Route>
         </Route>
       </Routes>
->>>>>>> Stashed changes
     </>
   );
 }
