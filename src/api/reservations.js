3252265dfe5662974c82ad3b0f9d97c83d8f84ce
Yesenia Ramirez <ramirezy0415@gmail.com>
2025-10-08 19:44:36 -0500

    feature: Adding functionality for reserve book

---
+++ src/api/reservations.js
@@ -0,0 +1,22 @@
+const API = import.meta.env.VITE_API;
+const BASE_RESERVATION_URL = `${API}/reservations`;
+
+export async function addReservation(token, bookId) {
+  if (!token) {
+    throw new Error("Please log in to reserve book");
+  }
+
+  const response = fetch(BASE_RESERVATION_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ bookId }),
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
--- src/reservations/BookDetail.jsx
+++ src/reservations/BookDetail.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { getBookDetails } from "../api/books";
+import { addReservation } from "../api/reservations";
 
 export default function BookDetail() {
   const navigate = useNavigate();
   const { id } = useParams();
+  const { token } = useState(null);
 
   const [selectedBook, setSelectedBook] = useState(null);
+  const [error, setError] = useState(null);
+
   const handleBackButton = () => {
     navigate("/books");
   };
@@ -19,8 +23,17 @@ export default function BookDetail() {
     syncBookDetails();
   }, [id]);
 
+  const handleReserveBook = async () => {
+    setError(null);
+    try {
+      await addReservation(token, id);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return selectedBook ? (
-    <section className="book-detail">
+    <section className="book-detail" key={selectedBook.id}>
       <button onClick={handleBackButton}>Back</button>
       <h2>{selectedBook.title}</h2>
       <h3>Author: {selectedBook.author}</h3>
@@ -28,7 +41,7 @@ export default function BookDetail() {
         src={selectedBook.coverimage}
         alt={`Cover of ${selectedBook.title}`}
       />
-      <p>
+      <p className="book-description">
         Description: {selectedBook.description}
         <br />
         {selectedBook.available ? "Available" : "Out of Stock"}
@@ -37,9 +50,11 @@ export default function BookDetail() {
         className={`book-reserve ${
           selectedBook.available ? " available" : " not-available"
         }`}
+        onClick={handleReserveBook}
       >
         Reserve Book
       </button>
+      {error && <p role="alert">{error}</p>}
     </section>
   ) : (
     <p>Please select a book to view more details</p>
